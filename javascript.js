6798ee220e1cb356a36da275e5c41c2861a5e9cf
Diana Gerardo <dianagerardo@email.arizona.edu>
2019-11-27 00:15:42 -0700

    Adding transitions

--- javascript.js
+++ javascript.js
@@ -25,31 +25,32 @@ window.onload = function () {
             });
         });
     });
-   
+
     // Transition example 2
 
-    const $img2 = document.querySelector('.image2');
-    const $logo = document.querySelector('.transition__logo');
-    const $frameBlack = document.querySelector('.page-transition__black');
-    const $frameRed = document.querySelector('.page-transition__red');
-    const $button = document.querySelector('#button');
+    const $button = $(".arrow");
 
-   
-    
-    let tltransition = new TimelineMax({paused:true})
-      .fromTo($frameRed , 2.2, {scaleX: 0},{scaleX: 1, transformOrigin:'left', ease: Power4.easeInOut},)
-      .fromTo($frameBlack , 2.2, {scaleX: 0},{scaleX: 1, transformOrigin:'left', ease: Power4.easeInOut},.2)
-      .fromTo($logo , 1.6, {xPercent: -100, autoAlpha:0 },{xPercent: 0, autoAlpha:1, ease: Power4.easeInOut},.7)
-      .set($frameRed, {scaleX:0})
-      .set($img2, {autoAlpha:0})
-      .to($frameBlack , 2.2, {scaleX: 0, transformOrigin:'right', ease: Power4.easeInOut})
-      .to($logo , .2, {autoAlpha:0 },'-=1.2')
-      
-    
-    
-    $button.addEventListener('click', () => {
-      tltransition.play(0);
-    });  
+    const $about = $(".transition-about");
+    const $portfolio = $(".transition-portfolio")
+
+
+
+    let transition = function () {
+        TweenLite.fromTo($about, 2.2, { scaleX: 0 }, { scaleX: 1, transformOrigin: 'left', ease: Power4.easeInOut })
+        TweenLite.fromTo($portfolio, 2.2, { scaleX: 0 }, { scaleX: 1, transformOrigin: 'left', ease: Power4.easeInOut }, .2)
+        TweenLite.fromTo($about, 1.6, { xPercent: -100, autoAlpha: 0 }, { xPercent: 0, autoAlpha: 1, ease: Power4.easeInOut }, .7)
+        TweenLite.set($about, { scaleX: 0 })
+        TweenLite.set($portfolio, { autoAlpha: 0 })
+        TweenLite.to($about, 2.2, { scaleX: 0, transformOrigin: 'right', ease: Power4.easeInOut })
+        TweenLite.to($about, .2, { autoAlpha: 0 }, '-=1.2')
+}
+
+
+
+
+$button.on('click', () => {
+    transition(0);
+});  
 
     
     
